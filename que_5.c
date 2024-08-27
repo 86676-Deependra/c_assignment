97b4c6bd4641c41cd0af1d102b64f6449d50c20b
drn-sharma99 <drn.sharma99@gmail.com>
2024-08-27 22:47:19 +0530

    que_5 uploaded

---
+++ que_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char input;
+    int uppercase = 0, lowercase = 0, digits = 0, others = 0;
+
+    printf("Enter characters (Press Enter to stop): \n");
+
+    while ((input = getchar()) != '\n') {
+        if (isupper(input)) {
+            uppercase++;
+        } else if (islower(input)) {
+            lowercase++;
+        } else if (isdigit(input)) {
+            digits++;
+        } else {
+            others++;
+        }
+    }
+
+    printf("\nCharacter Count:\n");
+    printf("Uppercase Letters: %d\n", uppercase);
+    printf("Lowercase Letters: %d\n", lowercase);
+    printf("Digits: %d\n", digits);
+    printf("Other Characters: %d\n", others);
+
+    return 0;
+}
